8c2ad2111453042cd9b1353fe6c00bc73e8eb4bd
David Liu <davidbliu@gmail.com>
2015-04-28 10:56:13 -0700

    added calendar pulling

--- app.py
+++ app.py
@@ -35,12 +35,29 @@ def points():
 	attendance_sums = np.sum(attendance_matrix, axis = 1)
 
 	sorted_mids = sorted(member_dict.keys(), key = lambda x: -attendance_sums[x])
-	print attendance_sums
 	return render_template('points.html', member_dict = member_dict, 
 		attendance_matrix = attendance_matrix,
 		attendance_sums = attendance_sums,
 		sorted_mids = sorted_mids)
 
+"""auth views"""
+import events.models as event_models
+
+@app.route("/login")
+def events():
+	flow = event_models.get_flow()
+	return '<a href = "' + event_models.get_auth_uri(flow)+ '">use google</a>'
+
+@app.route('/auth_return')
+def auth_return():
+	flow = event_models.get_flow()
+	code = request.args.get('code')
+	credentials = event_models.get_credentials(flow, code)
+	calendar = event_models.get_calendar(credentials)
+	return calendar['summary']
+	# credentials = event_models.get_credentials(flow, code)
+	# return event_models.get_calendar_data(credentials)
+	# return 'i dont know what to do mayne'
 
 if __name__ == "__main__":
 	# print dir(attendance)
--- events/models.py
+++ events/models.py
@@ -11,13 +11,33 @@ def test_calendar():
 	    'https://www.googleapis.com/auth/sqlservice.admin')
 	service = build('calendar', 'v3', developerKey = config.api_key, clientId = config.client_id, clientSecret = config.client_secret)
 
-def test_flow():
-	from oauth2client.client import flow_from_clientsecrets
-	flow = flow_from_clientsecrets('client_secrets.json',
-                               scope='https://www.googleapis.com/auth/calendar',
-                               redirect_uri='http://localhost:5000/auth_return')
+def get_flow():
+	# from oauth2client.client import flow_from_clientsecrets
+	from oauth2client.client import OAuth2WebServerFlow
+	flow = OAuth2WebServerFlow(client_id = config.client_id,
+									client_secret = config.client_secret,
+									scope = 'https://www.googleapis.com/auth/calendar',
+									redirect_uri='http://localhost:5000/auth_return')
 
-	auth_uri = flow.step1_get_authorize_url()
-	print auth_uri
+	
+	return flow
 
-test_flow()
+def get_auth_uri(flow):
+	return flow.step1_get_authorize_url()
+
+def get_credentials(flow, code):
+	credentials = flow.step2_exchange(code)
+	return credentials
+
+def get_calendar(credentials):
+	import httplib2
+	http = httplib2.Http()
+	http = credentials.authorize(http)
+	service = build('calendar', 'v3', http=http)
+	calendar = service.calendars().get(calendarId=pbl_calendar_id).execute()
+	return calendar
+
+if __name__=='__main__':
+	flow = get_flow()
+	credentials = get_credentials(flow, code)
+	get_calendar_data(credentials)
