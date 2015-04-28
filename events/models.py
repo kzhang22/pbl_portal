0beb9b63f58a0fcda1f038708c89c3cfae873866
David Liu <davidbliu@gmail.com>
2015-04-28 10:16:32 -0700

    added

--- events/models.py
+++ events/models.py
@@ -1,17 +1,3 @@
-
-"""
-
-550107127002-grljj2dso2phjvbdo02cem30kt1d6tqj.apps.googleusercontent.com
-Email address	
-550107127002-grljj2dso2phjvbdo02cem30kt1d6tqj@developer.gserviceaccount.com
-Client secret	
-z7HGMn9Z5QZu-B2bIHEoFIEZ
-Redirect URIs	
-https://www.example.com/oauth2callback
-Javascript Origins	
-https://www.example.com
-
-"""
 import config
 import json
 """pulls google events"""
@@ -19,15 +5,19 @@ from apiclient.discovery import build
 
 pbl_calendar_id = '8bo2rpf4joem2kq9q2n940p1ss@group.calendar.google.com'
 
-from oauth2client.appengine import AppAssertionCredentials
+def test_calendar():
+	from oauth2client.appengine import AppAssertionCredentials
+	credentials = AppAssertionCredentials(
+	    'https://www.googleapis.com/auth/sqlservice.admin')
+	service = build('calendar', 'v3', developerKey = config.api_key, clientId = config.client_id, clientSecret = config.client_secret)
 
-credentials = AppAssertionCredentials(
-    'https://www.googleapis.com/auth/sqlservice.admin')
+def test_flow():
+	from oauth2client.client import flow_from_clientsecrets
+	flow = flow_from_clientsecrets('client_secrets.json',
+                               scope='https://www.googleapis.com/auth/calendar',
+                               redirect_uri='http://localhost:5000/auth_return')
 
+	auth_uri = flow.step1_get_authorize_url()
+	print auth_uri
 
-
-service = build('calendar', 'v3', developerKey = config.api_key, clientId = config.client_id, clientSecret = config.client_secret)
-
-print dir(service.calendars().get(calendarId = pbl_calendar_id).execute())
-print '\n\n\n'
-print dir(service)
+test_flow()
