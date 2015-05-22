487957f0f228cd00c04d630fc7081a97d173e1cc
David Liu <davidbliu@gmail.com>
2015-05-22 13:30:57 -0700

    Delete fab_config.py

--- fab_config.py
+++
@@ -1,5 +0,0 @@
-amazon_key = 'AKIAJGFUI4L6P6CPLXGA'
-amazon_secret = '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'
-amazon_regions = ['us-west-2']
-keypair_location = '/Users/davidbliu/desktop/wd/boomboompow.pem'
-mesos_master_host = '52.11.149.15'
